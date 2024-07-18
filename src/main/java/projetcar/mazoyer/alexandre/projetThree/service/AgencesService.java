1b300183fe0673fee770e972cee77b4a5eeeeaf1
Mazoyer <mazoyeralexandre@hotmail.fr>
2024-07-18 22:02:40 +0200

    add table, controller, service,repository 'agences'

--- src/main/java/projetcar/mazoyer/alexandre/projetThree/service/AgencesService.java
+++ src/main/java/projetcar/mazoyer/alexandre/projetThree/service/AgencesService.java
@@ -1,10 +1,13 @@
 package projetcar.mazoyer.alexandre.projetThree.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import alex.mazoyer.blogue.globe.models.Article;
 import projetcar.mazoyer.alexandre.projetThree.models.Agences;
 import projetcar.mazoyer.alexandre.projetThree.repository.AgencesRepository;
 import projetcar.mazoyer.alexandre.projetThree.request.AgencesRequest;
@@ -30,7 +33,18 @@ public class AgencesService {
 		return new ResponseEntity<>(new MessageResponse("The agences" + agences.getName() + "was successfully added :"), HttpStatus.ACCEPTED);;
 	}
 	
-	public ResponseEntity<?> getAgences(AgencesRequest agencesRequest) throws Exception {
+	public ResponseEntity<?> getAgenceID(Long id) throws Exception {
+		
+		Agences agences = agencesRepository.findById(id)
+				.orElseThrow(() -> new RuntimeException("Error: Article is not found."));
+
+		return null;
+	}
+	
+	public ResponseEntity<?> getAgences() throws Exception {
+		
+		List<Agences> agences = agencesRepository.findAll();
+
 		return null;
 	}
 
